d32ea306e716be6821914635d0000c0fea4a80a9
ankita-78709 <ankitaraj655@gmail.com>
2021-04-16 20:32:38 +0530

    Updates

---
+++ precache-manifest.69cbbef72e426ef64aafe86835ead576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b26b48c1fd89203c276233bf1cf392",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "73c36115cd4368013457",
+    "url": "/portfolio/static/css/main.028c232e.chunk.css"
+  },
+  {
+    "revision": "0f6c2fe4a7acabf23520",
+    "url": "/portfolio/static/js/2.f06d4b79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.f06d4b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73c36115cd4368013457",
+    "url": "/portfolio/static/js/main.b85d0fb9.chunk.js"
+  },
+  {
+    "revision": "ac0cb4934f96d31d7b35",
+    "url": "/portfolio/static/js/runtime-main.7aea907c.js"
+  }
+]);
